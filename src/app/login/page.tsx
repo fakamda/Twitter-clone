45ee0454923cdb06c513f968f8a5bb2404f46816
fakamda <facundoelcardenal@gmail.com>
2023-11-25 21:17:48 -0300

    login with RSC

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { AuthButtonServer } from "@/app/components/auth-button-server";
+
+export default function Login() {
+    return (
+        <section className="grid place-content-center min-h-screen">
+            <h1 className="text-xl font-bold mb-4">Inicia sesion en Twister</h1>
+            <AuthButtonServer />
+        </section>
+
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
-import { AuthButton } from "./components/auth-button"
+import { AuthButtonServer } from "@/app/components/auth-button-server"
+import { redirect } from "next/navigation"
 
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('posts').select('*')
+  const { data: { session } } = await supabase.auth.getSession()
+  const { data: posts } = await supabase.from('posts').select()
+
+  if (session === null) {
+    redirect('/login')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButton />
+      <AuthButtonServer />
       Hola Twitter 👋🏻
       <pre>
         {
