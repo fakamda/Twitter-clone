1f1d3efb7425a7a3e8e92e5ebdc86ecbcfe82105
fakamda <facundoelcardenal@gmail.com>
2023-11-27 19:51:08 -0300

    asociando el user_id al twit para recuperar la info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,18 @@ import { redirect } from "next/navigation"
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
   const { data: { session } } = await supabase.auth.getSession()
-  const { data: posts } = await supabase.from('posts').select()
 
   if (session === null) {
     redirect('/login')
   }
+
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, users(name, avatar_url, username)') //esto es un join que lo hace automaticamente supabase detectando que con el user id popula el resto del objeto que tiene ese id
+
+  // como no permite crear triggers de tablas privadas en supabase ejecutamos este comando
+  // create trigger on_auth_insert_users after insert on auth.users for each row execute function insert_user_in_public_table_for_new_user();  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AuthButtonServer />
@@ -24,3 +31,5 @@ export default async function Home() {
     </main>
   )
 }
+
+
