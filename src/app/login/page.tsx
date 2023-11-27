6558cc0155bf34f8545086240c0c328a50e7cf3e
fakamda <facundoelcardenal@gmail.com>
2023-11-26 22:55:18 -0300

    adding user_id

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,6 @@ export default function Login() {
     return (
         <section className="grid place-content-center min-h-screen">
             <h1 className="text-xl font-bold mb-4">Inicia sesion en Twister</h1>
-
             <AuthButtonServer />
         </section>
 
