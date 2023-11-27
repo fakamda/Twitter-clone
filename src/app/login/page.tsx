8f21da738b8fb6f9aaa44c276efe35da1d5930b5
fakamda <facundoelcardenal@gmail.com>
2023-11-26 21:55:15 -0300

    a

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ export default function Login() {
     return (
         <section className="grid place-content-center min-h-screen">
             <h1 className="text-xl font-bold mb-4">Inicia sesion en Twister</h1>
+
             <AuthButtonServer />
         </section>
 
