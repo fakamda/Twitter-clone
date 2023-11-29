331474fdc902a749e6913d5fee0d9675120b00c9
fakamda <facundoelcardenal@gmail.com>
2023-11-28 23:45:32 -0300

    some fixes

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -10,13 +10,17 @@ export function ComposePost({
 }: {
     userAvatarUrl: string
 }) {
+    const addPost = async (formData: FormData) => {
+        'use server' //indica que este metodo solo se ejecuta en el servidor
+        console.log('holamundo')
+    }
     return (
-        <form className="flex flex-row space-x-4 p-4">
+        <form action={addPost} className="flex flex-row p-4 border-b border-white/20">
             <Avatar radius='full' size='md' src={userAvatarUrl} />
             <div className="flex flex-1 flex-col gap-y-4">
-                <textarea name="post" rows={4} className="w-full text-xl bg-black placeholder-gray-500 p-2 resize-none bg-focus" placeholder='Que esta pasando!?'>
+                <textarea name="post" rows={3} className="w-full text-xl bg-black placeholder-gray-500 p-2 resize-none focus:outline-none" placeholder='Que esta pasando!?'>
                 </textarea>
-                <button className="bg-sky-500 font-bold rounded-full px-5 py-2 self-end">
+                <button type="submit" className="bg-sky-500 font-bold rounded-full px-5 py-2 self-end">
                     Postear
                 </button>
             </div>
